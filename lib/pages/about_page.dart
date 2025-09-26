7d3f8df89246a9dab88aebb13e7cfd71feab203b
Rafael Ouverney <rafael_ouverney31@hotmail.com>
2025-09-25 22:00:56 -0300

    [FIX] Correção da app bar e adição de imagens ao pubspec

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
@@ -6,94 +7,140 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sobre'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar.medium(
+            pinned: true,
+            expandedHeight: 200,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            leading: IconButton(
+              icon: const Icon(Icons.chevron_left),
+              color: Colors.black,
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            flexibleSpace: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                child: Container(
+                  color: Colors.white.withValues(alpha: 0.5),
+                  padding: const EdgeInsets.all(16),
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: const Text(
+                      'Sobre',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      color: Colors.green[800],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    // Placeholder for the logo
-                    child: const Icon(
-                      Icons.code,
-                      color: Colors.white,
-                      size: 80,
+                  Center(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 120,
+                          height: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.green[800],
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Image.asset(
+                            'assets/logo-zenite.png',
+                            width: 200,
+                            height: 200,
+                            frameBuilder: (BuildContext context, Widget child, int? frame, bool wasSynchronouslyLoaded) {
+                              return ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: child,
+                              );
+                            },
+                            fit: BoxFit.contain,
+                            
+                            errorBuilder: (_, __, ___) => Icon(Icons.error, size: 100),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Desenvolvido pela Zênite-Digital',
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 24),
                   const Text(
-                    'Desenvolvido pela Zênite-Digital',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    'Equipe',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
+                  const SizedBox(height: 8),
+                  _buildTeamMember(
+                    name: 'Arthur Hydeki Ricken',
+                    role: 'Desenvolvedor',
+                    imageAsset: 'assets/profile-pics/1.png',
+                  ),
+                  _buildTeamMember(
+                    name: 'Eduardo B. Garcia',
+                    role: 'Desenvolvedor',
+                    imageAsset: 'assets/profile-pics/2.png',
+                  ),
+                  _buildTeamMember(
+                    name: 'Rafael L. B. Ouverney',
+                    role: 'Desenvolvedor',
+                    imageAsset: 'assets/profile-pics/3.png',
+                  ),
+                  _buildTeamMember(
+                    name: 'Vitor B. Garcia',
+                    role: 'Desenvolvedor',
+                    imageAsset: 'assets/profile-pics/4.png',
+                  ),
+                  const SizedBox(height: 24),
+                  const Text(
+                    'Licenças',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  _buildLicenseItem(
+                    title: 'Imagens de Perfil',
+                    subtitle: 'Imagem por pikisuperstar, Freepik',
+                    icon: Icons.collections,
+                  ),
+                  const SizedBox(height: 24),
+                  const Text(
+                    'IFPR',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  _buildLicenseItem(
+                    title: 'Desenvolvido no IFPR',
+                    subtitle:
+                        'Parte de projeto na disciplina de Desenvolvimento para Dispositivos Móveis',
+                    icon: Icons.school,
+                  ),
+                  const SizedBox(height: 32),
                 ],
               ),
             ),
-            const SizedBox(height: 24),
-            const Text(
-              'Equipe',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            _buildTeamMember(
-              name: 'Arthur Hydeki Ricken',
-              role: 'Desenvolvedor',
-              imageAsset: 'assets/profile-pics/1.png',
-            ),
-            _buildTeamMember(
-              name: 'Eduardo B. Garcia',
-              role: 'Desenvolvedor',
-              imageAsset: 'assets/profile-pics/2.png',
-            ),
-            _buildTeamMember(
-              name: 'Rafael L. B. Ouverney',
-              role: 'Desenvolvedor',
-              imageAsset: 'assets/profile-pics/3.png',
-            ),
-            _buildTeamMember(
-              name: 'Vitor B. Garcia',
-              role: 'Desenvolvedor',
-              imageAsset: 'assets/profile-pics/4.png',
-            ),
-            const SizedBox(height: 24),
-            const Text(
-              'Licenças',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            _buildLicenseItem(
-              title: 'Imagens de Perfil',
-              subtitle: 'Imagem por pikisuperstar, Freepik',
-              icon: Icons.collections,
-            ),
-            const SizedBox(height: 24),
-            const Text(
-              'IFPR',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            _buildLicenseItem(
-              title: 'Desenvolvido no IFPR',
-              subtitle:
-                  'Parte de projeto na disciplina de Desenvolvimento para Dispositivos Móveis',
-              icon: Icons.school,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:codex/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
@@ -7,61 +8,99 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Configurações'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: ListView(
-        children: [
-          _buildSettingsItem(
-            context,
-            icon: Icons.data_object,
-            title: 'Dados da API',
-            subtitle: 'Configuração da API',
-            onTap: () {
-              // TODO: Navigate to API settings
-            },
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar.medium(
+            pinned: true,
+            expandedHeight: 200,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            leading: IconButton(
+              icon: const Icon(Icons.chevron_left),
+              color: Colors.black,
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            flexibleSpace: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                child: Container(
+                  color: Colors.white.withOpacity(0.5),
+                  padding: const EdgeInsets.all(16),
+                  alignment: Alignment.bottomLeft,
+                  child: const Text(
+                    'Informações da sua API',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
-          _buildSettingsItem(
-            context,
-            icon: Icons.person_outline,
-            title: 'Dados do Usuário',
-            subtitle:
-                'Modificação de dados do usuário como Nome, E-mail e Senha',
-            onTap: () {
-              // TODO: Navigate to User Data
-            },
-          ),
-          _buildSettingsItem(
-            context,
-            icon: Icons.pie_chart_outline,
-            title: 'Visualização',
-            subtitle: 'Configuração dos gráficos da dashboard',
-            onTap: () {
-              // TODO: Navigate to Visualization settings
-            },
-          ),
-          _buildSettingsItem(
-            context,
-            icon: Icons.timer_outlined,
-            title: 'Tempo Limite',
-            subtitle:
-                'Configuração do tempo limite de resposta (apenas para visualização)',
-            onTap: () {
-              // TODO: Navigate to Timeout settings
-            },
-          ),
-          _buildSettingsItem(
-            context,
-            icon: Icons.info_outline,
-            title: 'Sobre',
-            subtitle: 'Sobre o Codex, licensas e equipe de desenvolvimento',
-            onTap: () {
-              Navigator.pushNamed(context, AppRoutes.about);
-            },
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  const SizedBox(height: 8),
+                  _buildSettingsItem(
+                    context,
+                    icon: Icons.data_object,
+                    title: 'Dados da API',
+                    subtitle: 'Configuração da API',
+                    onTap: () {
+                      // TODO: Navigate to API settings
+                    },
+                  ),
+                  _buildSettingsItem(
+                    context,
+                    icon: Icons.person_outline,
+                    title: 'Dados do Usuário',
+                    subtitle:
+                        'Modificação de dados do usuário como Nome, E-mail e Senha',
+                    onTap: () {
+                      // TODO: Navigate to User Data
+                    },
+                  ),
+                  _buildSettingsItem(
+                    context,
+                    icon: Icons.pie_chart_outline,
+                    title: 'Visualização',
+                    subtitle: 'Configuração dos gráficos da dashboard',
+                    onTap: () {
+                      // TODO: Navigate to Visualization settings
+                    },
+                  ),
+                  _buildSettingsItem(
+                    context,
+                    icon: Icons.timer_outlined,
+                    title: 'Tempo Limite',
+                    subtitle:
+                        'Configuração do tempo limite de resposta (apenas para visualização)',
+                    onTap: () {
+                      // TODO: Navigate to Timeout settings
+                    },
+                  ),
+                  _buildSettingsItem(
+                    context,
+                    icon: Icons.info_outline,
+                    title: 'Sobre',
+                    subtitle: 'Sobre o Codex, licensas e equipe de desenvolvimento',
+                    onTap: () {
+                      Navigator.pushNamed(context, AppRoutes.about);
+                    },
+                  ),
+                  const SizedBox(height: 32),
+                ],
+              ),
+            ),
           ),
         ],
       ),
@@ -76,13 +115,14 @@ class SettingsPage extends StatelessWidget {
     required VoidCallback onTap,
   }) {
     return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
       leading: Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: Colors.purple.withOpacity(0.1),
+          color: Colors.blue.withValues(alpha: 0.1),
           borderRadius: BorderRadius.circular(10),
         ),
-        child: Icon(icon, color: Colors.purple),
+        child: Icon(icon, color: Colors.blueAccent),
       ),
       title: Text(title),
       subtitle: Text(subtitle),
